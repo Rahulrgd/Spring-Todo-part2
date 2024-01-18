13e708c8cdf15d36d006155ff79843e1da98f5b2
RahulGupta <rahulrgd21@gmail.com>
2024-01-18 23:54:47 +0530

    Step 02 - Call Spring Boot Hello World REST API from React Hello World Component

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -11,6 +13,13 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
-	
+	@Bean
+	public WebMvcConfigurer crosConfigurer(){
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 }
