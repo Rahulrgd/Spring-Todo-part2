3f23768fd89a13f4ed34ed701f988a5ffb532164
RahulGupta <rahulrgd21@gmail.com>
2024-01-20 02:36:10 +0530

    Completed Step 15 - Adding Update Todo and Create Todo REST API to Spring Boot Backend API

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -9,17 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RestfulWebServicesApplication.class, args);
+  }
 
-	@Bean
-	public WebMvcConfigurer crosConfigurer(){
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
-
+  @Bean
+  public WebMvcConfigurer crosConfigurer() {
+    return new WebMvcConfigurer() {
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedMethods("*")
+          .allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
 }
